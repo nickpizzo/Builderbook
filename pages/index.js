62862c22d0165de58aa9564b1acf41150dc0a267
Nick Pizzo <nick.pizzo@gmail.com>
2018-11-17 21:04:52 -0800

    connect server, User model schema

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
+import PropTypes from 'prop-types';
 import Head from 'next/head';
-import Button from '@material-ui/core/Button';
 import withLayout from '../lib/withLayout';
 
-const Index = () => (
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index Page</title>
       <meta name="description" content="This is the description of the Index page" />
     </Head>
     <p>Index page content</p>
-    <Button variant="contained" color="primary">
-      MUI Button
-    </Button>
+    <p>{user.email}</p>
   </div>
 );
+
+Index.getInitialProps = async ({ query }) => ({ user: query.user });
+
+Index.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+Index.defaultProps = {
+  user: null,
+};
 
 export default withLayout(Index);
